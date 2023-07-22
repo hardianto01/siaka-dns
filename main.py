7360327d9cc01df5a7b7f487620b589e3d409aed
udin <antojago5@gmail.com>
2023-07-23 00:35:21 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests
+from tabulate import tabulate
+
+print("Ispector Siaka By Hardianto!!!!!!")
+
+email = int(input('Masukan NIM: '))
+pasw = input('Masukan password: ')
+
+payLoad = {
+        'email': email,
+        'pasw': pasw,
+        'juser': 'MHS'
+    }
+
+def getCookie():
+    
+    data = requests.post('https://siaka.undipa.ac.id/index.php?page=login&aksi=masuk', data=payLoad)
+    return data.headers['Set-Cookie']
+
+def getDns():
+    headers = {
+        'cookie': getCookie()
+    }
+    data = requests.get('https://siaka.undipa.ac.id/index.php?page=dns', headers=headers)
+    soup = BeautifulSoup(data.content, 'html.parser')
+    datas = []
+    table = soup.find_all("table")[2]
+    for row in table.find_all("tr"):
+        row_data = [cell.get_text(strip=True).replace("\t", " ") for cell in row.find_all(["td"])]
+        datas.append(row_data)
+    for i in range(len(datas)):
+        if (i == 4): continue
+        if (i == 21): continue
+        else: 
+            print(' '.join(datas[i]))
+
+getDns()
---
+++ readme.md
@@ -0,0 +1,9 @@
+`CARA MENGGUNAKAN: `
+
+```shell
+$ pip install -r requirements.text
+```
+
+`Running `
+```shell
+python main.py```
